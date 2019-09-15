e2d7eb234edf9568b9d513338682c1439a8fc2f2
Amanda Young <amanda80@ksu.edu>
2019-09-15 14:12:37 -0500

    steakburger, wrap done

--- Menu/Entrees/SteakosaurusBurger.cs
+++ Menu/Entrees/SteakosaurusBurger.cs
@@ -7,13 +7,9 @@ namespace DinoDiner.Menu.Entrees
     public class SteakosaurusBurger
     {
         private bool bun = true;
-        private bool lettuce = true;
-        private bool tomato = true;
-        private bool onion = true;
         private bool pickle = true;
         private bool ketchup = true;
         private bool mustard = true;
-        private bool mayo = true;
 
         public double Price { get; set; }
         public uint Calories { get; set; }
@@ -22,17 +18,39 @@ namespace DinoDiner.Menu.Entrees
         {
             get
             {
-                List<string> ingredients = new List<string>() { "3 Steakburger Patties" };
+                List<string> ingredients = new List<string>() { "Steakburger Patty" };
                 if (bun) ingredients.Add("Bread");
-                if (lettuce) ingredients.Add("Lettuce");
-                if (tomato) ingredients.Add("Tomato");
-                if (onion) ingredients.Add("Onion");
                 if (pickle) ingredients.Add("Pickles");
                 if (ketchup) ingredients.Add("Ketchup");
                 if (mustard) ingredients.Add("Mustard");
-                if (mayo) ingredients.Add("Mayo");
                 return ingredients;
             }
+        }
+
+        public SteakosaurusBurger()
+        {
+            this.Price = 5.15;
+            this.Calories = 621;
+        }
+
+        public void HoldBun()
+        {
+            this.bun = false;
+        }
+
+        public void HoldPickle()
+        {
+            this.pickle = false;
+        }
+
+        public void HoldKetchup()
+        {
+            this.ketchup = false;
+        }
+
+        public void HoldMustard()
+        {
+            this.mustard = false;
         }
     }
 }
