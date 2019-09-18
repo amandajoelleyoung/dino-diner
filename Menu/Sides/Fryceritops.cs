ce46a2d0c8eccbea632a5b70abee1dc6928316fa
Amanda Young <amanda80@ksu.edu>
2019-09-18 15:23:50 -0500

    fryceritops

--- Menu/Sides/Fryceritops.cs
+++ Menu/Sides/Fryceritops.cs
@@ -4,12 +4,46 @@ using System.Text;
 
 namespace DinoDiner.Menu.Sides
 {
-    public class Fryceritops
+    public class Fryceritops : Side
     {
-        private Side fries;
+        public override List<string> Ingredients
+        {
+            get
+            {
+                return new List<string>() { "Potato", "Salt", "Vegetable Oil" };
+            }
+        } 
+        private Size size;
+        public override Size Size
+        {
+            set
+            {
+                size = value;
+                switch (size)
+                {
+                    case Size.Large:
+                        Price = 1.95;
+                        Calories = 480;
+                        break;
+                    case Size.Medium:
+                        break;
+                    case Size.Small:
+                        Price = .99;
+                        Calories = 222;
+                        break;
+                }
+            }
+            get
+            {
+                return size;
+            }
+        }
+
         public Fryceritops()
         {
-            
+            this.Price = 0.99;
+            this.Calories = 222;
+          //this.Ingredients = new List<string>() { "Potato", "Salt", "Vegetable Oil" };
         }
     }
 }
