fd2065bd8eef8aab9834ce55b2d914a76080b838
Amanda Young <amanda80@ksu.edu>
2019-12-02 22:45:37 -0600

    mm2

--- Website/Pages/Menu.cshtml.cs
+++ Website/Pages/Menu.cshtml.cs
@@ -12,9 +12,24 @@ namespace Website.Pages
     public class MenuModel : PageModel
     {
         public Menu Menu { get; } = new Menu();
+   
+        [BindProperty]
+        public string search { get; set; }
+
+        [BindProperty]
+        public List<string> menutype { get; set; } = new List<string>();
+
         public void OnGet()
         {
 
+        }
+
+        public void OnPost()
+        {
+            if (search != null)
+            {
+               
+            }
         }
     }
 }
